b7c99b696c708028257cf71f2194640a4d3865bd
LedinN <nick.ledin@hotmail.com>
2024-09-23 17:10:39 +0200

    add: ratelimiter config and controller implementation

--- src/main/java/com/nick/webserviceproject/controller/WeatherController.java
+++ src/main/java/com/nick/webserviceproject/controller/WeatherController.java
@@ -3,9 +3,13 @@ package com.nick.webserviceproject.controller;
 import com.nick.webserviceproject.model.current.WeatherDataCurrent;
 import com.nick.webserviceproject.model.forecast.WeatherDataForecast;
 import com.nick.webserviceproject.service.WeatherService;
+import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
+import io.github.resilience4j.reactor.ratelimiter.operator.RateLimiterOperator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
@@ -16,9 +20,11 @@ import java.util.Optional;
 public class WeatherController {
 
     private final WeatherService weatherService;
+    private final RateLimiter rateLimiter;
 
-    public WeatherController(WeatherService weatherService) {
+    public WeatherController(WeatherService weatherService, RateLimiter rateLimiter) {
         this.weatherService = weatherService;
+        this.rateLimiter = rateLimiter;
     }
 
     @GetMapping("/current")
@@ -37,10 +43,16 @@ public class WeatherController {
         }
 
         return weatherService.getAndSaveCurrentWeather(location)
+                .transformDeferred(RateLimiterOperator.of(rateLimiter))
                 .map(ResponseEntity::ok)
                 .defaultIfEmpty(ResponseEntity.notFound().build())
                 .onErrorResume(e -> {
-                    return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build());
+                    if (e instanceof RequestNotPermitted) {
+                        return Mono.just(ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                                .body(null));
+                    } else {
+                        return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build());
+                    }
                 });
     }
 
@@ -50,6 +62,7 @@ public class WeatherController {
             @RequestParam Optional<String> city,
             @RequestParam Optional<Double> lat,
             @RequestParam Optional<Double> lon) {
+
         String location;
         if (lat.isPresent() && lon.isPresent()) {
             location = lat.get() + "," + lon.get();
@@ -60,11 +73,16 @@ public class WeatherController {
         }
 
         return weatherService.getAndSaveForecastWeather(location)
+                .transformDeferred(RateLimiterOperator.of(rateLimiter))
                 .map(ResponseEntity::ok)
                 .defaultIfEmpty(ResponseEntity.notFound().build())
                 .onErrorResume(e -> {
+                    if (e instanceof RequestNotPermitted) {
+                        return Mono.just(ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                                .body(null));
+                    } else {
                     return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build());
-                });
+                }});
     }
 
 
