8c93bd93670708784d58d753ac83f8c693cce383
Nick <nick.ledin@hotmail.com>
2024-11-20 13:21:20 +0100

    add: basic securityfilterchain

--- src/main/java/com/nick/webserviceproject/config/security/AppSecurityConfig.java
+++ src/main/java/com/nick/webserviceproject/config/security/AppSecurityConfig.java
@@ -1,4 +1,36 @@
 package com.nick.webserviceproject.config.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class AppSecurityConfig {
+
+    private final CustomUserDetailsService userDetailsService;
+
+    public AppSecurityConfig(CustomUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeRequests(auth -> auth
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/weather/*").hasRole("USER")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+
+
+    }
 }
