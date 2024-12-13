e3859e779940f14a4d7666837ffbfbd32a10d956
LedinN <nick.ledin@hotmail.com>
2024-12-13 09:55:06 +0100

    Remove: ContextLoads test

--- src/test/java/com/nick/webserviceproject/WebServiceProjectApplicationTests.java
+++ src/test/java/com/nick/webserviceproject/WebServiceProjectApplicationTests.java
@@ -1,13 +1,9 @@
 package com.nick.webserviceproject;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest()
 class WebServiceProjectApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
